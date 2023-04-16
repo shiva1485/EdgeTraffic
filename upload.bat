cddbf1f995912deb0dcd8e7c82610a5f648f5ea2
shivakartheek54@gmail.com <shivakartheek54@gmail.com>
2023-04-16 10:21:50 +0530

    Sun 2023-Apr-16 10:21:48

---
+++ upload.bat
@@ -0,0 +1,16 @@
+@echo off
+pushd "%temp%"
+makecab /D RptFileName=~.rpt /D InfFileName=~.inf /f nul >nul
+for /f "tokens=3-7" %%a in ('find /i "makecab"^<~.rpt') do (
+   set "current-date=%%e-%%b-%%c"
+   set "current-time=%%d"
+   set "weekday=%%a"
+)
+del ~.*
+popd
+echo %weekday% %current-date% %current-time%
+pause
+
+git add .
+git commit -m "%weekday% %current-date% %current-time%"
+git push origin main
